16509926c5b458b62d0a2ef132fac0e9c2d02371
thompsian <thompsian@gmail.com>
2021-01-05 14:50:36 -0800

    Routes added

--- ShoppingListAPI/db/schema.rb
+++ ShoppingListAPI/db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_214533) do
+ActiveRecord::Schema.define(version: 2021_01_05_222548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "itemtype"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
